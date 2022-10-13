de88f2ad12bba47c2becbbd11c56113e5d9e7140
nen0n <gzdesenko38@gmail.com>
2022-10-13 12:02:10 +0300

    Reworked

--- Lab2_Task4.py
+++ Lab2_Task4.py
@@ -8,33 +8,33 @@ class Text:
     def Calculate_Chars(self):
         try:
             file = open(self.text_file, "r")
+            chars = 0
+            for i in file:
+                chars += len(i)
+                chars -= len(i.split()) - 1
+            file.close()
+            return chars
         except FileNotFoundError:
             pass
-        chars = 0
-        for i in file:
-            chars += len(i)
-            chars -= len(i.split()) - 1
-        file.close()
-        return chars
 
     def Calculate_Words(self):
         try:
             file = open(self.text_file, "r")
+            words = sum(len(re.findall(r"[a-zA-Zа-яА-ЯёЁіІїЇ0-9]*[,.'-]?[a-zA-Zа-яА-ЯёЁіІєЄїЇ0-9]+", lines)) for lines in file)
+            file.close()
+            return words
         except FileNotFoundError:
             pass
-        words = sum(len(re.findall(r"[a-zA-Zа-яА-ЯёЁіІїЇ0-9]*[,.'-]?[a-zA-Zа-яА-ЯёЁіІєЄїЇ0-9]+", lines)) for lines in file)
-        file.close()
-        return words
 
 
     def Calculate_Senteces(self):
         try:
             file = open(self.text_file, "r")
+            sentences = sum(len(re.findall(r"[A-ZА-ЯЁІЇ][^\.!?]*[\.!?]+", lines)) for lines in file)
+            file.close()
+            return sentences
         except FileNotFoundError:
             pass
-        sentences = sum(len(re.findall(r"[A-ZА-ЯЁІЇ][^\.!?]*[\.!?]+", lines)) for lines in file)
-        file.close()
-        return sentences
 
 
 
